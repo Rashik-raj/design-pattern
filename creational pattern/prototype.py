b13cadeddb532f6b7af40b1846b4db1b959a9119
Rashik-raj <rashik123.rs@gmail.com>
2020-05-30 10:10:29 +0545

    prototype design pattern completed

---
+++ creational pattern/prototype.py
@@ -0,0 +1,29 @@
+import copy
+
+class Prototype:
+    def __init__(self):
+        self._objects = dict()
+
+    def register_object(self,name,obj):
+        self._objects[name] = obj
+    
+    def unregister_object(self,name):
+        del self._objects[name]
+
+    def clone(self,name,**attrs):
+        return copy.deepcopy(self._objects.get(name))
+
+class Car:
+    def __init__(self):
+        self.name = 'Skylark'
+        self.color = 'Red'
+        self.options = 'Ex'
+
+    def __str__(self):
+        return '{} | {} | {}'.format(self.name,self.color,self.options)
+
+c = Car()
+prototype = Prototype()
+prototype.register_object('Skylark',c)
+clone = prototype.clone('Skylark')
+print(clone)
