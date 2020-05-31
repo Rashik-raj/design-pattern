d57535ba21069895073b65d42e2c5156f624393d
Rashik-raj <rashik123.rs@gmail.com>
2020-05-31 08:33:08 +0545

    Observer design pattern completed

--- creational pattern/singleton.py
+++ creational pattern/singleton.py
@@ -11,7 +11,7 @@ class Borg:
 
 class Singleton(Borg):
     def __init__(self,**kwargs):
-        # Borg.__init__(self)
+        Borg.__init__(self)
         self._shared_state.update(kwargs)
 
     def __str__(self):
